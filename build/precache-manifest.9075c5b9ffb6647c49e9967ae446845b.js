3ce31e484eda17cf8dc35930d6684a7b42605fb4
dietBlue <jeff.charters@gmail.com>
2020-04-15 11:38:21 -0400

    serve static from build

---
+++ build/precache-manifest.9075c5b9ffb6647c49e9967ae446845b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38a92d25a44373c61c07af2e41cc347",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39be6e0b7b8d7e071293",
+    "url": "/static/css/main.306a1ea3.chunk.css"
+  },
+  {
+    "revision": "f5ffb0dc48ad9952bcfd",
+    "url": "/static/js/2.5c68fe71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c68fe71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39be6e0b7b8d7e071293",
+    "url": "/static/js/main.dc1639fd.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('body', (req, res) => {
 
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 app.use(express.json())
 
 let persons = [
